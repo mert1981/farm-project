8f5ea21576f7f251502445119ee12fb4191b69e3
mert1981 <mertkoc1981@gmail.com>
2022-01-02 22:31:54 +0300

    Anasayfa temel kısımlar yapıldı

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,66 @@
 <template>
-  <div class="home">
+  <div class="home ">
+    <div class="h-screen">
+		<div class="h-full flex flex-col float-right w-3/12  shadow-2xl">
+			<div class="min-h-screen flex flex-col max-w-md mx-auto bg-gray-200 opacity-100 font-poppins px-4 bg-no-repeat bg-cover bg-center">
+  <div class="flex justify-between px-1 pt-4 items-center">
     <div>
-    <div class="w-9/12 text-left h-24  mt-4 ml-4">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M10.828 12l4.95 4.95-1.414 1.414L8 12l6.364-6.364 1.414 1.414z"/></svg>
+    </div>
+    <div>
+      <p class="font-semibold">My Profile</p>
+    </div>
+    <div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 3c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 14c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0-7c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"/></svg>
+    </div>
+  </div>
+  <div class="flex items-center px-4 pt-12 justify-between">
+    <div class="w-18 h-24 bg-blue-600 flex items-center rounded-full">
+      <img class="h-20 w-20 mx-auto" src="https://lnmlpexiwaspywjbwwvd.supabase.in/storage/v1/object/sign/assets/boy.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJhc3NldHMvYm95LnBuZyIsImlhdCI6MTYzMzUzMzk2NywiZXhwIjoxOTQ4ODkzOTY3fQ.HL6DqbdJMCZI6dqEN-ZQAu5EwtblW7r8YNuud4_kHV8">
+    </div>
+    <div class="w-8/12 flex items-center">
+      <div class="w-10/12 flex flex-col leading-none pl-4">
+        <p class="text-2xl font-bold">Soefyan</p>
+        <p class="text-sm pt-1 font-light text-gray-700">Network Engineer</p>
+      </div>
+      <div class="w-2/12">
+        <div>
+          <svg xmlns="http://www.w3.org/2000/svg" class="text-gray-700" fill="currentColor" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M9.243 19H21v2H3v-4.243l9.9-9.9 4.242 4.244L9.242 19zm5.07-13.556l2.122-2.122a1 1 0 0 1 1.414 0l2.829 2.829a1 1 0 0 1 0 1.414l-2.122 2.121-4.242-4.242z"/></svg>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="pt-12 px-4 w-full flex flex-col">
+    <p class="font-semibold text-gray-600">My status</p>
+    <div class="flex w-full pt-2 space-x-2">
+      <button class="bg-gray-800 w-32 rounded-full px-4 py-2 font-ligth text-white flex">🤠 Away</button>
+      <button class="bg-green-800 w-32 rounded-full px-4 py-2 font-ligth text-white flex">🎮 Game</button>
+      <button class="bg-red-800 w-32 rounded-full px-4 py-2 font-ligth text-white flex">👷 Work</button>
+    </div>
+  </div>
+  <div class="pt-12 px-4 w-full flex flex-col">
+    <p class="font-semibold text-gray-600">Dashboard</p>
+    <div class="flex flex-col w-full pt-2 space-y-2">
+      <div class="flex w-full h-12">
+        <div class="w-2/12 h-full">
+          <div class="h-12 w-12 bg-green-600 rounded-full flex items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" class="mx-auto text-white" fill="currentColor" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M2 9h19a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V9zm1-6h15v4H2V4a1 1 0 0 1 1-1zm12 11v2h3v-2h-3z"/></svg>
+          </div>
+        </div>
+        <div class="w-6/12 h-full flex items-start">
+          <p class="my-auto text-lg font-semibold">Payment</p>
+        </div>
+        <div class="w-4/12 h-full flex justify-end items-end">
+          <button class="flex bg-red-600 rounded-md my-auto px-5 py-1 float-right text-white font-medium">2 New</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+	</div>
+    <div class="w-8/12 text-left h-24  mt-4 ml-4">
+	
     <div class="relative text-gray-600 float-right mt-8 mr-4">
   <input type="search" name="serch" placeholder="Search" class="bg-gray-200 h-10 px-5 pr-10 rounded-full text-sm focus:outline-none">
   <button type="submit" class="absolute right-0 top-0 mt-3 mr-4">
@@ -17,7 +76,8 @@
   
     
     </div>
-    <div class="w-9/12 h-96 mt-6 ml-4 ">
+	
+    <div class="w-8/12 h-96 mt-6 ml-4 ">
     <!--
 This is one of the HTML templates to be available on;
 https://www.pagesection.com/templates
@@ -124,7 +184,7 @@ SVG/Icons credit to https://icons.getbootstrap.com
 					<path d="M9.929 4.929H11.929V8.929H9.929z" transform="rotate(-45 10.935 6.931)"></path>
 				</svg></div>
 		</div>
-		<div class="text-md pt-4 pb-4 px-4">
+		<div class="text-md pt-4 pb-4 px-2">
 			<div class="flex justify-between items-center">
 				<div class="space-y-2">
 					<span class="flex space-x-2 items-center"><svg height="20" width="20" viewBox="0 0 32 32" class="fill-current"><path d="M13,30a5.0057,5.0057,0,0,1-5-5h2a3,3,0,1,0,3-3H4V20h9a5,5,0,0,1,0,10Z"></path><path d="M25 25a5.0057 5.0057 0 01-5-5h2a3 3 0 103-3H2V15H25a5 5 0 010 10zM21 12H6V10H21a3 3 0 10-3-3H16a5 5 0 115 5z"></path></svg> <span>27km/h</span></span><span class="flex space-x-2 items-center"><svg height="20" width="20" viewBox="0 0 32 32" class="fill-current"><path d="M16,24V22a3.2965,3.2965,0,0,0,3-3h2A5.2668,5.2668,0,0,1,16,24Z"></path><path d="M16,28a9.0114,9.0114,0,0,1-9-9,9.9843,9.9843,0,0,1,1.4941-4.9554L15.1528,3.4367a1.04,1.04,0,0,1,1.6944,0l6.6289,10.5564A10.0633,10.0633,0,0,1,25,19,9.0114,9.0114,0,0,1,16,28ZM16,5.8483l-5.7817,9.2079A7.9771,7.9771,0,0,0,9,19a7,7,0,0,0,14,0,8.0615,8.0615,0,0,0-1.248-3.9953Z"></path></svg> <span>32%</span></span>
@@ -139,7 +199,7 @@ SVG/Icons credit to https://icons.getbootstrap.com
     </div>
 </section>
     </div>
-    <div class="w-9/12 h-96 bg-black mt-6 ml-4">
+    <div class="w-8/12 h-96 bg-black mt-4 ml-4">
     
     </div>
     <div class="">
@@ -161,3 +221,11 @@ export default {
   components: {},
 };
 </script>
+<style>
+.bg-gelap{
+  background-color: #001E26;
+}
+.bg-btn{
+  background-color: #FF6446;
+}
+</style>
